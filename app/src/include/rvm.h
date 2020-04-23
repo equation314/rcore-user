a808d82ecdc50c93c961b8482dcc7c5e343de277
equation314 <equation618@gmail.com>
2020-04-24 00:32:43 +0800

    RVM: update RVM_VCPU_CREATE interface

--- app/src/include/rvm.h
+++ app/src/include/rvm.h
@@ -6,4 +6,9 @@
 #define RVM_VCPU_CREATE (RVM_IO + 0x11)
 #define RVM_VCPU_RESUME (RVM_IO + 0x12)
 
+struct rvm_vcpu_create_args {
+    unsigned short vmid;
+    unsigned long long entry;
+};
+
 #endif // RVM_H
--- app/src/vmm.c
+++ app/src/vmm.c
@@ -14,15 +14,17 @@ int main()
 {
     int fd = open("/dev/rvm", O_RDWR);
     int vmid = ioctl(fd, RVM_GUEST_CREATE, GUEST_MEM_SZ);
-    void *guest_phys_mem_ptr = (char *)mmap(0, GUEST_MEM_SZ, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    int vcpu_id = ioctl(fd, RVM_VCPU_CREATE, vmid);
+    void *guest_phys_mem_ptr = mmap(0, GUEST_MEM_SZ, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    struct rvm_vcpu_create_args args = {vmid, 0x8000};
+    int vcpu_id = ioctl(fd, RVM_VCPU_CREATE, &args);
 
     printf("fd = %d\n", fd);
     printf("vmid = %d\n", vmid);
     printf("guest_phys_mem_ptr = %p\n", guest_phys_mem_ptr);
     printf("vcpu_id = %d\n", vcpu_id);
 
-    if (guest_phys_mem_ptr != -1)
+    if (guest_phys_mem_ptr != (void*)-1)
         munmap(guest_phys_mem_ptr, GUEST_MEM_SZ);
     close(fd);
     return 0;
