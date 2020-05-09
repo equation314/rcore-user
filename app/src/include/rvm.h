5a1fab7c29ecb3aca933e627cf48da8256f7dccb
chenyu <believe.chenyu@gmail.com>
2020-05-09 21:19:45 +0800

    add rvm command RVM_GUEST_ACCESS_MEMORY

--- app/src/include/rvm.h
+++ app/src/include/rvm.h
@@ -2,6 +2,7 @@
 #define RVM_H
 
 #define RVM_IO 0xAE00
+#define RVM_GUEST_ACCESS_MEMORY (RVM_IO + 0x02)
 #define RVM_GUEST_CREATE (RVM_IO + 0x01)
 #define RVM_VCPU_CREATE (RVM_IO + 0x11)
 #define RVM_VCPU_RESUME (RVM_IO + 0x12)
--- app/src/vmm.c
+++ app/src/vmm.c
@@ -13,19 +13,41 @@ const int GUEST_MEM_SZ = 16 * 1024 * 1024; // 16 MiB
 int main()
 {
     int fd = open("/dev/rvm", O_RDWR);
+    printf("rvm fd = %d\n", fd);
+
     int vmid = ioctl(fd, RVM_GUEST_CREATE, GUEST_MEM_SZ);
-    void *guest_phys_mem_ptr = mmap(0, GUEST_MEM_SZ, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("vmid = %d\n", vmid);
+
+    void *guest_phys_mem_ptr = ioctl(fd, RVM_GUEST_ACCESS_MEMORY, vmid);
+    printf("guest_phys_mem_ptr = %p\n", guest_phys_mem_ptr);
+
+    {
+        int* mem_test = (int*)guest_phys_mem_ptr;
+        for (int i = 0; i < 100; i ++) {
+            mem_test[i] = i;
+        }
+        int error = 0;
+        for (int i = 0; i < 100; i ++) {
+            if (mem_test[i] != i) {
+                printf("mem test failed\n");
+                error = 1;
+                break;
+            }
+        }
+        if (!error) {
+            printf("mem test success\n");
+        }
+    }
 
     struct rvm_vcpu_create_args args = {vmid, 0x8000};
     int vcpu_id = ioctl(fd, RVM_VCPU_CREATE, &args);
 
-    printf("fd = %d\n", fd);
-    printf("vmid = %d\n", vmid);
-    printf("guest_phys_mem_ptr = %p\n", guest_phys_mem_ptr);
     printf("vcpu_id = %d\n", vcpu_id);
 
-    if (guest_phys_mem_ptr != (void*)-1)
-        munmap(guest_phys_mem_ptr, GUEST_MEM_SZ);
+    // void *guest_phys_mem_ptr = mmap(0, GUEST_MEM_SZ, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    // printf("guest_phys_mem_ptr = %p\n", guest_phys_mem_ptr);
+    // if (guest_phys_mem_ptr != (void*)-1)
+    //     munmap(guest_phys_mem_ptr, GUEST_MEM_SZ);
     close(fd);
     return 0;
 }
